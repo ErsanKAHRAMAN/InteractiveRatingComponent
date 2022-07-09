b847d9cf681e0ee5dcba2cb5744e3c5ebc6015e3
Ersan Kahraman <87430770+ErsanKAHRAMAN@users.noreply.github.com>
2022-07-09 20:59:55 +0300

    Add files via upload

---
+++ InteractiveRatingComponent/app.js
@@ -0,0 +1,34 @@
+const ratingElement = document.querySelector(".container");
+const submitElement = document.querySelector(".btn");
+const thankElement = document.querySelector(".success");
+const ratingİnfo = document.querySelector(".rating-button");
+
+
+submitElement.addEventListener("click",onSubmit);
+
+function onSubmit(e){
+ 
+   
+    document.querySelector(".container").style.display = 'none';
+    document.querySelector(".success").style.display = 'flex';
+
+
+}
+
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
